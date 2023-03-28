ae0184bc65de45ee5101294cdc648dfcb3a31d7b
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2023-03-28 06:03:40 +0000

    rimer push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+/* import fondo from 'https://wallpaperaccess.com/full/3693955.jpg'; */
+import Base from './componentes/base'
+
+function App() {
+  return (
+    <div className='App'>
+      <Base />
+    </div>
+  );
+}
+export default App;
---
+++ src/componentes/base.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function base() {
+  return (
+    <div className='base'>
+
+    </div>
+  )
+}
+
+export default base
