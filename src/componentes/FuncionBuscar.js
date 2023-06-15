0d491482139a02cbcecad904c5b1d65f50f4f970
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2023-06-14 22:30:53 -0400

    14-06-23

--- src/componentes/FuncionBuscar.js
+++ src/componentes/FuncionBuscar.js
@@ -1,21 +1,42 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react'
+import { Modal, Typography } from '@mui/material';
 
 function FuncionBuscar({nombre}) {
 
     const [imageUrl, setImageUrl] = useState(null);
-    
+
+    //manipulación del modal
+    const [abrirModal, setAbrirModal] = useState(false);
+    const funcionAbrirModal = () => setAbrirModal(true);
+    const funcionCerrarModal = () => setAbrirModal(false);
+
     const Url = 'https://pokeapi.co/api/v2/pokemon/' + nombre
 
+    /*Cada vez que algo se actualice en el DOM se ejecutará esta función. */
     useEffect(() => {
-        
-        fetch( Url )
-        .then(response => response.json())
-        .then( data => {
-            setImageUrl(data.sprites.front_default)
-        } )
-        .catch(() => console.log("Problemas al comunicarse con el servidor para obtener Pkm_img."))
 
+        fetch( Url )
+        .then((response) =>{ 
+
+          if( !response.ok ){
+
+            {funcionAbrirModal()}
+            <Modal
+              open={abrirModal}
+              onClose={funcionCerrarModal}
+            >
+              <Box>
+                <Typography> modal </Typography>
+              </Box>
+            </Modal>
+
+          }
+          return response.json();
+        }).then( data => {
+          setImageUrl(data.sprites.front_default)
+        } )
+      .catch(() => console.log("Problemas al comunicarse con el servidor para obtener Pkm_img."))
     });
 
 
