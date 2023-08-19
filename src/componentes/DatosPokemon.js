8c65d291fb6d5f0eb8d998510aef89c4cb451fd3
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2023-08-19 08:12:45 -0400

    19-08-2023

--- src/componentes/DatosPokemon.js
+++ src/componentes/DatosPokemon.js
@@ -1,52 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 
-function DatosPokemon({nombre}) {
+function DatosPokemon({ nombre }) {
+  const [nombrePokemon, setNombreUrl] = useState(null);
+  const [tipoPokemon, setTipoUrl] = useState(null);
+  const [numeroPokemon, setNumeroUrl] = useState(null);
+  const [habilidades, setHabilidades] = useState(null);
+  const [peso, setPeso] = useState(null);
 
-    const[nombrePokemon, setNombreUrl] = useState(null);
-    const[tipoPokemon, setTipoUrl] = useState(null);
-    const[numeroPokemon, setNumeroUrl] = useState(null);
+  const Url = "https://pokeapi.co/api/v2/pokemon/" + nombre;
 
-    const Url = 'https://pokeapi.co/api/v2/pokemon/' + nombre
-    const urlLocation = "https://pokeapi.co/api/v2/location/" + "25"
 
-    useEffect(() => {
-          
-        fetch( Url )
-        .then((response) =>{
+  useEffect(() => {
+    fetch(Url)
+      .then((response) => {
+        if (!response.ok) {
+          setNombreUrl("???");
+          setTipoUrl("???");
+          setNumeroUrl("???");
+        }
 
-          if( !response.ok ){
-            setNombreUrl("???");
-            setTipoUrl("???");
-            setNumeroUrl("???");
-          }
-          
-          return response.json();
-        }).then( data => {
-            setNombreUrl(data.name);
-            setTipoUrl(data.types[0].type.name);
-            setNumeroUrl(data.id);
-        })
-        .catch(() => console.log("Error al buscar nombre del Pokémon"))
-        
-        fetch(urlLocation)
-        .then((responseLocation) =>{
-          //console.log(responseLocation.region)
-          console.log("URL: "+ urlLocation)
-          console.log(responseLocation)
-        
-        })
-
-        })
+        return response.json();
+      })
+      .then((data) => {
+        setNombreUrl(data.name); //NOMBRE
+        setTipoUrl(data.types[0].type.name); // TIPO
+        setNumeroUrl(data.id); //ID
+        setHabilidades(data.abilities[0].ability.name) //HABILIDADES
+        setPeso(data.weight) //Peso
+      })
+      .catch(() => console.log());
+  });
 
   return (
-    <Box className='DatosPokemon' sx={{ m:0 }}>
+    <Box className="DatosPokemon" sx={{ m: 0 }}>
       <h1>#{numeroPokemon}</h1>
       <h1>{nombrePokemon}</h1>
       <h1>{tipoPokemon} type</h1>
-      <h1 className='location'>LOCATION</h1>
-      <h1>Route 104</h1>
+      <h1>{peso} LB</h1>
+      <h1 className="habilidades">Main Ability</h1>
+      <h1>{habilidades}</h1>
     </Box>
-  )
+  );
 }
-export default DatosPokemon
+export default DatosPokemon;
