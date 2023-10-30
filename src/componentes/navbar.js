93e68c152968fdac96a3c09e790d4dd60fc98d88
Rodolfo Cuevas <rcuevaspantoja@gmail.com>
2023-10-30 17:37:11 -0300

    finale

--- src/componentes/navbar.js
+++ src/componentes/navbar.js
@@ -7,7 +7,7 @@ function navbar() {
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <Link class="navbar-brand text-light" as={Link} to={"/"} >Pokedex
           </Link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
